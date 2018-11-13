68374b60ad2e526a004710740b5373b6757724b9
Orlando Vargas <OrlandoV87@live.com>
2018-11-12 19:41:17 -0500

    Remove: The argc argument.

--- project2.c
+++ project2.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
-<<<<<<< HEAD
 #include "item.h"
 
-int main(int argc, char *argv[])
-{
+int main(char *argv[]) {
+
 
     //TODO: Receive data
 
     //TODO: Send data.
 
+
     return 0;
-=======
-
-int main(int argc, char *argv[]){
-
->>>>>>> 99617e3b12ff07877110c2cfa8da296af9358146
 }
