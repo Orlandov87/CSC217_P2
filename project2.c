601f532da80008fbd3f64664521d5ed0cbc2e4ef
Orlando Vargas <OrlandoV87@live.com>
2018-11-12 20:01:42 -0500

    Added: getMyLine function

--- project2.c
+++ project2.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "item.h"
 
-int main(char *argv[]) {
-
+int main() {
 
     //TODO: Receive data
 
     //TODO: Send data.
 
-
     return 0;
+}
+
+int getMyLine(char s[], int lim){
+    int c, i;
+
+    i = 0;
+    while(--lim > 0 && (c = getchar()) != EOF && c != '\n') {
+        s[i++] = c;
+    }
+    if(c == '\n') {
+        s[i++] = c;
+    }
+    s[i] = '\0';
+    return i;
 }
