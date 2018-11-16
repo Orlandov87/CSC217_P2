4d275c2333c989d9a64363fef91fcae0b3351020
Orlando <Orlandov87@live.com>
2018-11-16 02:46:20 -0500

    Became 2-space guy

--- project2.c
+++ project2.c
@@ -3,23 +3,20 @@
 
 int main() {
 
-    //TODO: Receive data
 
-    //TODO: Send data.
-
-    return 0;
+  return 0;
 }
 
 int getMyLine(char s[], int lim){
-    int c, i;
+  int c, i;
 
-    i = 0;
-    while(--lim > 0 && (c = getchar()) != EOF && c != '\n') {
-        s[i++] = c;
-    }
-    if(c == '\n') {
-        s[i++] = c;
-    }
-    s[i] = '\0';
-    return i;
+  i = 0;
+  while(--lim > 0 && (c = getchar()) != EOF && c != '\n') {
+    s[i++] = c;
+  }
+  if(c == '\n') {
+    s[i++] = c;
+  }
+  s[i] = '\0';
+  return i;
 }
