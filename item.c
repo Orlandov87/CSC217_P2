2fbd329fc99acdd719ac34d817bcfa966e4e5fdf
Orlando Vargas <OrlandoV87@live.com>
2018-12-03 18:44:13 -0500

    Update: The strcmp to strncmp since strcmp was depricated

--- item.c
+++ item.c
@@ -106,9 +106,9 @@ int compareTo(Node* node1, Node* node2) {
         return 1;
     }
     if (node1->sku != NULL && node2->sku != NULL) {
-        return strcmp(node1->sku, node2->sku);
+        return strncmp(node1->sku, node2->sku, MAX_CHARS);
     } else if (node1->name != NULL && node2->name != NULL) {
-        if (strcmp(node1->name, node2->name) == 0) {
+        if (strncmp(node1->name, node2->name, MAX_CHARS) == 0) {
             return 0;
         }
     }
