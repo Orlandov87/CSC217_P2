4d8022a391ad808fbe0d058ec5d7ef751cf23124
Orlando Vargas <OrlandoV87@live.com>
2018-12-04 22:08:01 -0500

    Add: Files that will take care of the sales of the program

---
+++ sale.h
@@ -0,0 +1,16 @@
+/*
+ * Author: Orlando Vargas
+ * Language: C
+ */
+
+/*
+ * Search the list for the sku, if it finds it, it returns the address
+ * of the node, NULL otherwise.
+ */
+Node* searchSKU(Node*, String);
+
+/*
+ * Takes in the sku, the quantaty, the tax, the list of nodes and the
+ */
+
+void buy(String, int);
