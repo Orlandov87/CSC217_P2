563d19992b98086e631cc9e1288a20851f668af3
Orlando Vargas <OrlandoV87@live.com>
2018-12-06 21:15:12 -0500

    Add: Buy function to process the sales.

--- sale.c
+++ sale.c
@@ -8,8 +8,35 @@
 // larger than itself.
 Node* searchSKU(Node* list, String sku) {
     Node* temp = list;
-    while (strncmp(temp->sku, sku, MAX_CHARS) != 0 && temp != NULL) {
+
+    // Realized to put strcmp after
+    while (temp != NULL && strcmp(temp->sku, sku) != 0) {
         temp = temp->next;
     }
     return temp;
+}
+
+/*
+ *
+ */
+// TODO: This can be programmed more efficiently.
+void buy(String sku, int quantity, Node* list) {
+    Node* temp = searchSKU(list, sku);
+    if (temp == NULL || temp->quantity == 0) {
+        printf("Sku %s not in stock\n", sku);
+    } else if (temp->quantity < quantity) {
+        float priceAsF = strtof(temp->price, NULL);
+        priceAsF = priceAsF * (temp->quantity);
+        printf("Requested %d %s; only %d in stock\n", quantity, temp->name,
+               temp->quantity);
+        printf("%d x %s @ $%s: $%0.2f\n", temp->quantity, temp->name,
+               temp->price, priceAsF);
+        temp->quantity = 0;
+    } else {
+        float priceAsF = strtof(temp->price, NULL);
+        priceAsF = priceAsF * quantity;
+        temp->quantity = temp->quantity - quantity;
+        printf("%d x %s @ $%s: $%0.2f\n", quantity, temp->name, temp->price,
+               priceAsF);
+    }
 }
